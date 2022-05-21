4d1a7d4c59b663e19e643c27b3519e63dd68b7c4
Chaoran Chen <mail@chaoran-chen.de>
2022-05-21 19:31:20 +0200

    Add initial React code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>This is Mpox-Spectrum.</h1>
+    </div>
+  );
+}
+
+export default App;

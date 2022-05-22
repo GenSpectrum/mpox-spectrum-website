713c1f9bff710e74afea8538fc6d6ecec26232ef
Chaoran Chen <mail@chaoran-chen.de>
2022-05-22 11:13:41 +0200

    Improve CountryTable

--- src/widgets/CountryTable.tsx
+++ src/widgets/CountryTable.tsx
@@ -1,26 +1,35 @@
 import { CountryCountSampleDataset } from '../data/CountryCountSampleDataset';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 type Props = {
   countryCounts: CountryCountSampleDataset;
 };
 
 export const CountryTable = ({ countryCounts }: Props) => {
+  const columns: GridColDef[] = [
+    { field: 'country', headerName: 'Country', minWidth: 250 },
+    { field: 'count', headerName: 'Number sequences', minWidth: 200 },
+  ];
+
+  const rows = countryCounts.payload.map(({ country, count }, index) => ({
+    id: index,
+    country: country ?? 'Unknown',
+    count,
+  }));
+
   return (
-    <>
-      <table>
-        <thead>
-          <th>Country</th>
-          <th>Number sequences</th>
-        </thead>
-        <tbody>
-          {countryCounts.payload.map(({ country, count }) => (
-            <tr key={country}>
-              <td>{country ?? 'Unknown'}</td>
-              <td>{count}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        density={'compact'}
+        hideFooterPagination={true}
+        initialState={{
+          sorting: {
+            sortModel: [{ field: 'count', sort: 'desc' }],
+          },
+        }}
+      />
+    </div>
   );
 };
